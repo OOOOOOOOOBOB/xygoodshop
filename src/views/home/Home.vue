7c5d2c5fe0b2451b3ea8dd79c6ea5d8c7f68e90a
OOOOOOOOOBOB <1328979214@qq.com>
2020-06-28 15:55:13 +0800

    ceshi

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,9 +2,16 @@
     <!--主页-->
 
     <div class="home">
+        <div class="header">
+            <van-search placeholder="请输入搜索关键词" @click="handSearchClick"/>
+        </div>
 
         <div class="header">
             <van-search placeholder="请输入搜索关键词" @click="handSearchClick"/>
+        </div>
+
+        <div class="ee">
+            211212
         </div>
         <div class="home-content">
             <van-pull-refresh class="content"
